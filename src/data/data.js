9f83f8a798e3fd14e078ddcab4882949ca50a9b6
Jetty Prakash <prakash.jetty@compassitesinc.com>
2014-02-27 20:50:41 +0530

    Data plug-in commits

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+'use strict';
+var fs = require('jsdoc/fs');
+
+exports.defineTags = function(dictionary) {
+
+    /** 
+     *set data property of doclets for displaying chart data
+     */
+    dictionary.defineTag('data', {
+        mustHaveValue: true,
+        onTagged: function(doclet, tag) {
+            doclet.dataPresent = true;
+            if (tag.value) {
+              var re = /"([^"\\]*("|\\[\S\s]))+/g; 
+	            var dataFilePath=tag.value.match(re).toString().replace(/["']/g, "");
+              var dataArgString=tag.value.match('/{(.*)}/');
+              doclet.htmlId = Math.ceil((Math.random()*1000)+1);
+	             var args = fs.readFileSync(dataFilePath,'utf-8');
+              doclet.dataId= dataFilePath.split('.')[0]; 
+              doclet.dataSrc = args.replace(new RegExp('{capture}','g'),function(capture) {                       
+                       return doclet.dataId;
+                });
+              if (dataArgString) {
+	             doclet.dataAttr = JSON.parse(dataArgString);
+             }
+        	} 
+        }
+    });   
+
+};
